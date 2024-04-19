ef3c6a53bae86e4e57c15f0f1b96359b2ebcb6a4
vasilije.pecanin <Vasilije.Pecanin@htlstp.at>
2024-04-19 11:02:15 +0200

    vaske

--- src/main/java/level_1/Level_1.java
+++ src/main/java/level_1/Level_1.java
@@ -1,4 +1,61 @@
 package level_1;
 
+import java.io.*;
+import java.util.*;
+
 public class Level_1 {
+
+    public static void main(String[] args) {
+        try {
+            // Eingabetextdatei lesen
+            File inputFile = new File("C:\\Users\\vasil\\htlstp\\CCC 2024\\ccc-school-19-04-2024\\src\\main\\java\\level_1\\level1_example.in");
+            Scanner scanner = new Scanner(inputFile);
+
+            // Anzahl der Währungen lesen
+            int numCurrencies = scanner.nextInt();
+
+            // Anzahl der Münzen pro Währung in die Ausgabetextdatei schreiben
+            File outputFile = new File("output.txt");
+            FileWriter writer = new FileWriter(outputFile);
+
+            // Anzahl der Währungen in die Ausgabetextdatei schreiben
+            writer.write(numCurrencies + "\n");
+
+            // Die Anzahl der Münzen für jede Währung aus der Eingabetextdatei lesen und in die Ausgabetextdatei schreiben
+            for (int i = 0; i < numCurrencies; i++) {
+                int numCoinsPerCurrency = scanner.nextInt();
+                int[] coins = new int[numCoinsPerCurrency];
+                for (int j = 0; j < numCoinsPerCurrency; j++) {
+                    coins[j] = scanner.nextInt();
+                }
+                int result = findSmallestAmount(coins);
+                writer.write(result + "\n");
+            }
+
+            // Ressourcen freigeben
+            scanner.close();
+            writer.close();
+
+            System.out.println("Output erfolgreich in output.txt geschrieben.");
+
+        } catch (IOException e) {
+            System.out.println("Fehler beim Lesen oder Schreiben der Datei: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    // Methode, um den kleinsten Betrag zu finden, der nicht mit einer einzelnen Münze bezahlt werden kann
+    private static int findSmallestAmount(int[] coins) {
+        Arrays.sort(coins);
+        int smallestAmount = 1;
+
+        for (int coin : coins) {
+            if (coin > smallestAmount) {
+                break;
+            }
+            smallestAmount += coin;
+        }
+
+        return smallestAmount;
+    }
 }
--- src/main/java/level_2/Level_2.java
+++ src/main/java/level_2/Level_2.java
@@ -1,4 +1,32 @@
 package level_2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Level_2 {
+    public static void main(String[] args) {
+        int[][] currencies = {{1, 2, 6, 9, 14}, {1, 2, 3, 4, 7}, {1, 3, 5, 8, 11}};
+        int[][] amounts = {{10, 4, 15}, {7, 11, 10}, {12, 14, 6}};
+        findCoins(currencies, amounts);
+    }
+
+    public static void findCoins(int[][] currencies, int[][] amounts) {
+        for (int i = 0; i < currencies.length; i++) {
+            for (int amount : amounts[i]) {
+                for (int coin1 : currencies[i]) {
+                    for (int coin2 : currencies[i]) {
+                        if (coin1 + coin2 == amount) {
+                            System.out.println(coin1 + " " + coin2);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
